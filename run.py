a771ddc28871c4014e9ca4656b7687da4be0185d
Jacob Molsby <jacobmolsby@hotmail.se>
2021-07-17 12:37:29 +0000

    Deploying project to heroku

--- run.py
+++ run.py
@@ -1,9 +1,12 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
 
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 # @ == decorator
@@ -30,11 +33,15 @@ def about_member(member_name):
         for obj in data: 
             if obj["url"] == member_name:
                 member = obj
-    return "<h1>" + member["name"] + "</h1>"
+    # first member == member.html, the second member is the empty object declared in the beggining of this function
+    return render_template("member.html", member=member)
 
-
-@app.route("/contact")
+# to comply with the pep8 compliance of line not longer than 79char, enter just after opening braces.
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message!".format(
+            request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
 
@@ -47,6 +54,6 @@ def careers():
 if __name__ == "__main__":
     app.run(
         host=os.environ.get("IP", "0.0.0.0"),
-        port=int(os.environ.get("PORT","5000")),
+        port=int(os.environ.get("PORT", "5000")),
         debug=True)
     
